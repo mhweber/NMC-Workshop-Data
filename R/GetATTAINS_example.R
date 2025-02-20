5328824fd95cfdea606f6147bc8f28b29b666d0b
hillarymarler <Marler.Hillary@epa.gov>
2025-02-20 15:07:53 -0500

    Fix GetATTAINS data to match NMC workshop vignette

--- R/GetATTAINS_example.R
+++ R/GetATTAINS_example.R
@@ -3,6 +3,41 @@ UpdateGetATTAINSData <- function(){
   
   GreenBay_FoxRiver <- EPATADA::TADA_AutoClean(GreenBay_FoxRiver)
     
+  GreenBay_FoxRiver <- EPATADA::TADA_FindPotentialDuplicatesSingleOrg(GreenBay_FoxRiver)
+    
+  GreenBay_FoxRiver <- dplyr::filter(GreenBay_FoxRiver, TADA.SingleOrgDup.Flag == "Unique")
+    
+    
+    
+  GreenBay_FoxRiver <- EPATADA::TADA_SimpleCensoredMethods(GreenBay_FoxRiver, nd_method = "multiplier", 
+                                                    nd_multiplier = 0.5, od_method = "as-is", 
+                                                    od_multiplier = "null")
+    
+    
+  GreenBay_FoxRiver <- EPATADA::TADA_FindPotentialDuplicatesMultipleOrgs(GreenBay_FoxRiver)
+    
+  GreenBay_FoxRiver <- dplyr::filter(GreenBay_FoxRiver, TADA.ResultSelectedMultipleOrgs == "Y")
+    
+  GreenBay_FoxRiver <- EPATADA::TADA_RunKeyFlagFunctions(GreenBay_FoxRiver, clean = TRUE)
+    
+  GreenBay_FoxRiver <- EPATADA::TADA_AutoFilter(GreenBay_FoxRiver)
+    
+  GreenBay_FoxRiver <- EPATADA::TADA_FlagAboveThreshold(GreenBay_FoxRiver, clean = FALSE, 
+                                                 flaggedonly = FALSE)
+    
+  GreenBay_FoxRiver <- EPATADA::TADA_FlagBelowThreshold(GreenBay_FoxRiver, clean = FALSE, 
+                                                 flaggedonly = FALSE)
+
+    
+  GreenBay_FoxRiver <- EPATADA::TADA_HarmonizeSynonyms(GreenBay_FoxRiver)
+    
+  GreenBay_FoxRiver <- EPATADA::TADA_CalculateTotalNP(GreenBay_FoxRiver, daily_agg = "max")
+  
+  GreenBay_FoxRiver <- EPATADA::TADA_FlagCoordinates(GreenBay_FoxRiver, clean_outsideUSA = "change sign", 
+                       clean_imprecise = FALSE)
+  
+  GreenBay_FoxRiver <- GreenBay_FoxRiver[!is.na(GreenBay_FoxRiver$LongitudeMeasure),]
+    
   GreenBay_FoxRiver <- GreenBay_FoxRiver %>%
       dplyr::filter(TADA.ComparableDataIdentifier %in%
                       c("SPECIFIC CONDUCTANCE_NA_NA_US/CM", 
@@ -11,36 +46,8 @@ UpdateGetATTAINSData <- function(){
                         "TOTAL PHOSPHORUS, MIXED FORMS_UNFILTERED_AS P_UG/L",
                         "DISSOLVED OXYGEN (DO)_NA_NA_MG/L"))
     
-    GreenBay_FoxRiver <- EPATADA::TADA_FindPotentialDuplicatesSingleOrg(GreenBay_FoxRiver)
-    
-    GreenBay_FoxRiver <- dplyr::filter(GreenBay_FoxRiver, TADA.SingleOrgDup.Flag == "Unique")
-    
-    
-    
-    GreenBay_FoxRiver <- EPATADA::TADA_SimpleCensoredMethods(GreenBay_FoxRiver, nd_method = "multiplier", 
-                                                    nd_multiplier = 0.5, od_method = "as-is", 
-                                                    od_multiplier = "null")
-    
-    
-    GreenBay_FoxRiver <- EPATADA::TADA_FindPotentialDuplicatesMultipleOrgs(GreenBay_FoxRiver)
-    
-    GreenBay_FoxRiver <- dplyr::filter(GreenBay_FoxRiver, TADA.ResultSelectedMultipleOrgs == "Y")
-    
-    GreenBay_FoxRiver <- EPATADA::TADA_RunKeyFlagFunctions(GreenBay_FoxRiver, clean = TRUE)
-    
-    GreenBay_FoxRiver <- EPATADA::TADA_AutoFilter(GreenBay_FoxRiver)
-    
-    GreenBay_FoxRiver <- EPATADA::TADA_FlagAboveThreshold(GreenBay_FoxRiver, clean = FALSE, 
-                                                 flaggedonly = FALSE)
-    
-    GreenBay_FoxRiver <- EPATADA::TADA_FlagBelowThreshold(GreenBay_FoxRiver, clean = FALSE, 
-                                                 flaggedonly = FALSE)
-    
-    GreenBay_FoxRiver <- EPATADA::TADA_HarmonizeSynonyms(GreenBay_FoxRiver)
-    
-    GreenBay_FoxRiver <- EPATADA::TADA_CalculateTotalNP(GreenBay_FoxRiver, daily_agg = "max")
-    
-    WQP_with_ATTAINSonly <- EPATADA::TADA_GetATTAINS(GreenBay_FoxRiver, fill_catchments = FALSE, return_sf = TRUE)
+    WQP_with_ATTAINSonly <- EPATADA::TADA_GetATTAINS(GreenBay_FoxRiver, fill_catchments = FALSE, 
+                                                     return_sf = TRUE)
     
     rm(GreenBayFoxRiver)
     
